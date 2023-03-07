a6d2f9407b9f55980fc622da83e5c105cdc3ca9a
Panormamix <yophoenix38@gmail.com>
2023-03-07 17:52:23 +0100

    compte rendu 5-1

--- S09-10/TACHE5-1/test_contour.c
+++ S09-10/TACHE5-1/test_contour.c
@@ -9,14 +9,14 @@ int main(int argc, char** argv){
 		return 1;
 	}
 	Image I = lire_fichier_image(argv[1]);
-	printf("Recuperation du contour de l'image test :\n");
+	//printf("Recuperation du contour de l'image test :\n");
 	printf("nom du fichier: %s\n", argv[1]);
 	printf("hauteur image: %u\n", hauteur_image(I));
 	printf("largeur image: %u\n", largeur_image(I));
 	char* nom_fichier=modifier_extension(argv[1],"txt");
 	Liste_Contour L = extraire_les_contours(I);
 	affiche_liste_contour(L);
-	printf("Ecriture du contour dans le fichier texte : %s\n",nom_fichier);
-	ecrire_liste_contours(L,nom_fichier);
+	//printf("Ecriture du contour dans le fichier texte : %s\n",nom_fichier);
+	//ecrire_liste_contours(L,nom_fichier);
 	return 0;
 }
