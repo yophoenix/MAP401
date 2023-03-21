27a9738360633ecd48ca34f87c9985ed688178a7
Panormamix <yophoenix38@gmail.com>
2023-03-21 16:41:17 +0100

    Tache 6-2 (marche pas)

---
+++ S11/TACHE6-2/contour.h
@@ -0,0 +1,46 @@
+#ifndef _CONTOUR_H_
+#define _CONTOUR_H_
+
+#include "image.h"
+#include "types_macros.h"
+#include "liste_chainee.h"
+
+typedef enum {Nord, Est, Sud, Ouest} Orientation;
+
+Contour recupere_contour(Image I, Image M, Point pixel_init);
+
+void calcul_contour(Contour liste);
+
+void affiche_liste_contour(Liste_Contour liste);
+
+Point trouve_pixel_init(Image I, Point pixel);
+
+void memoriser_position(Liste_Point *liste, Point p);
+
+Point avancer(Point p, Orientation orient);
+
+Orientation nouvelle_orientation (Image I, Point position, Orientation orient);
+
+Orientation tourner_a_gauche(Orientation orient);
+
+Orientation tourner_a_droite(Orientation orient);
+
+void ecrire_contour_fichier(Contour L,FILE * f);
+
+void ecrire_liste_contours(Liste_Contour L, char *nom_fichier);
+
+void ecrire_contour_eps(Contour L, char *nom_fichier, Image I, int fill);
+
+void ecrire_image_eps(Liste_Contour L, char* nom_fichier, Image I);
+
+char *modifier_extension(char *nom,char *extension);
+
+char *stroke_ou_fill(char* nom,int fill);
+
+Image init_masque(Image M);
+
+Liste_Contour extraire_les_contours(Image I);
+
+Contour simplification_contour(Contour contour, UINT j1, UINT j2, UINT dist)
+
+#endif
