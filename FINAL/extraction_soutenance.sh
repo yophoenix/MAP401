c2ea4386b3c93989c2b8bc476c59c64ec139c182
Nozi <remi.salon.13@gmail.com>
2023-05-09 10:26:40 +0200

    Le shell pour tous (et pas le mariage)

--- FINAL/extraction_soutenance.sh
+++ FINAL/extraction_soutenance.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/sh
 
 vertclair='\e[1;32m'
 jaune='\e[1;33m'
@@ -8,11 +8,13 @@ neutre='\e[0;m'
 
 echo ""
 echo "Tests sur l'extraction de contour(s) à partir de 3 images PBM"
-read -p "Appuyez sur une touche pour continuer..."
+read -p "Appuyez sur une touche pour continuer..." attendre
+echo ""
 for img in "Asterix3.pbm" "lettre-L-cursive.pbm" "image_poly_tache6.pbm"; do
 	echo "Image traitée : ${rougefonce}$img${neutre}"
 	echo "./extraction_contour ../IMAGES_TESTS/$img"
 	./extraction_contour ../IMAGES_TESTS/$img
 	echo "[${vertclair}OK${neutre}]"
-	read -p "Appuyez sur une touche pour continuer..."
+	read -p "Appuyez sur une touche pour continuer..." attendre
+	echo ""
 done
